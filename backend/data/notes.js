86f1fd0ddd5fa7babca515caff7215c3323d7fb4
niv <nivroz93@gmail.com>
2021-07-15 15:37:23 +0300

    initialized repository

---
+++ backend/data/notes.js
@@ -0,0 +1,30 @@
+const notes = [
+  {
+    _id: "1",
+    title: "Day 1 of college",
+    content:
+      "I made a few new friends and introduced myself to a lot of new teachers.",
+    category: "College",
+  },
+  {
+    _id: "2",
+    title: "Learned some Node JS",
+    content: "Learned hot to create a server in node JS and my first API",
+    category: "Learning",
+  },
+  {
+    _id: "3",
+    title: "Watched some Anime",
+    content: "Finished 2 seasons of attack on titan and my hero academia.",
+    category: "Entertainment",
+  },
+  {
+    _id: "4",
+    title: "Started React JS",
+    content:
+      "Made my first app in react JS, feels awesome to learn somthing new",
+    category: "Learning",
+  },
+];
+
+module.exports = notes;
